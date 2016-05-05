e089b3e1929b711f51af38fe19de08d53055016c
reverinu <reverinudog@yahoo.co.jp>
2016-05-05 16:22:24 +0900

    EnemyMotion実装。

---
+++ KawakenBattleSystem/Assets/Scripts/EnemyMotion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMotion : MonoBehaviour {
+
+    private float charaSizeX;
+    private float charaSizeY;
+
+    void Start()
+    {
+        charaSizeX = this.GetComponent<RectTransform>().sizeDelta.x;
+    }
+
+	
+    void FixedUpdate()
+    {
+        charaSizeY = Mathf.PingPong(Time.time * 2, 1) * 5 + this.GetComponent<RectTransform>().sizeDelta.x;
+
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(charaSizeX, charaSizeY);
+    }
+}

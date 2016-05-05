cbd2368e75470857642dfeef41532a0cc187d78c
reverinu <reverinudog@yahoo.co.jp>
2016-05-05 12:04:38 +0900

    SkillScrollView簡易実装。UI若干変更。SkillButtonScriptの外枠だけ生成

---
+++ KawakenBattleSystem/Assets/Scripts/SkillButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SkillButtonScript : MonoBehaviour {
+
+    //[SerializeField][Header("キャラリスト")]
+    //private List<GameObject> charaList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
